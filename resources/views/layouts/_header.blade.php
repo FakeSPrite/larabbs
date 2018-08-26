7f455b96dfd441c3960c649174eb87a6bdc7f11c
Anvis <anvis89@gmail.com>
2018-08-26 20:44:53 +0000

    Move user model to models folder

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -36,7 +36,6 @@
                             </span>
                             {{ Auth::user()->name }} <span class="caret"></span>
                         </a>
-
                         <ul class="dropdown-menu" role="menu">
                             <li>
                                 <a href="{{ route('logout') }}"
