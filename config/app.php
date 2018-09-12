ffdc41938c45a63d3dbd7295b220ebdbdf7efdd9
Anvis <anvis89@gmail.com>
2018-09-13 07:17:59 +1000

    List topics created by user

--- config/app.php
+++ config/app.php
@@ -8,7 +8,7 @@ return [
     |utes/web.php
     | This value is the name of your application. This value is used when the
     | framework needs to place the application's name in a notification or
-    | any other location as required by the application or its packages.
+    | any other location as required by the application or its packages.ndex.blade.php
     |
     */
 
